98a08bcc98b7780d58c6578ff0a6d1ebabcaab01
Renato Groffe <renato.groffe@yahoo.com.br>
2023-08-28 17:11:52 -0300

    .NET 7 + Console Application + Azure Text Translation + ConsoleTools + Sharprompt + HttpClient
    .NET 7 + Console Application + Azure Text Translation + ConsoleTools + Sharprompt + HttpClient

---
+++ ConsoleAppTranslator/Inputs/InputHelper.cs
@@ -0,0 +1,101 @@
+﻿using DustInTheWind.ConsoleTools.Controls.InputControls;
+using Sharprompt;
+
+namespace ConsoleAppTranslator.Inputs;
+
+public static class InputHelper
+{
+    private const string EndpointTextTranslation = "https://api.cognitive.microsofttranslator.com/";
+    private const string RegionTextTranslation = "eastus";
+    private const string EnglishLanguage = "Ingles";
+    private const string PortugueseLanguage = "Portugues";
+    private const string SpanishLanguage = "Espanhol";
+    private const string EnglishCode = "en";
+    private const string PortugueseCode = "pt";
+    private const string SpanishCode = "es";
+
+    public static string GetEndpointTextTranslation()
+    {
+        var inputEndpointTextTranslation = new StringValue(
+            $"Endpoint Text Translation Service (default -> {EndpointTextTranslation}):");
+        inputEndpointTextTranslation.DefaultValue = EndpointTextTranslation;
+        inputEndpointTextTranslation.AcceptDefaultValue = true;
+        string endpointTextTranslation;
+        do
+        {
+            endpointTextTranslation = inputEndpointTextTranslation.Read();
+        } while (String.IsNullOrWhiteSpace(endpointTextTranslation));
+        return endpointTextTranslation;
+    }
+
+    public static string GetRegionTextTranslation()
+    {
+        var inputRegionTextTranslation = new StringValue(
+            $"Region do Azure (default -> {RegionTextTranslation}):");
+        inputRegionTextTranslation.DefaultValue = RegionTextTranslation;
+        inputRegionTextTranslation.AcceptDefaultValue = true;
+        string regionTextTranslation;
+        do
+        {
+            regionTextTranslation = inputRegionTextTranslation.Read();
+        } while (String.IsNullOrWhiteSpace(regionTextTranslation));
+        return regionTextTranslation;
+    }
+
+    public static string GetSubscriptionKey()
+    {
+        var inputSubscriptionKey = new StringValue("Subscription Key:");
+        string subscriptionKey;
+        do
+        {
+            subscriptionKey = inputSubscriptionKey.Read();
+            if (String.IsNullOrWhiteSpace(subscriptionKey))
+                ShowErrorMessage("Informe uma Subscription Key valida para o recurso!");
+        } while (String.IsNullOrWhiteSpace(subscriptionKey));
+        return subscriptionKey;
+    }
+
+    public static string GetTextToTranslate()
+    {
+        var inputTextToTranslate = new StringValue("Texto para traducao:");
+        string textToTranslate;
+        do
+        {
+            textToTranslate = inputTextToTranslate.Read();
+            if (String.IsNullOrWhiteSpace(textToTranslate))
+                ShowErrorMessage("Informe um texto valido para traducao!");
+        } while (String.IsNullOrWhiteSpace(textToTranslate));
+        return textToTranslate;
+    }
+
+    public static string GetTargetLanguage() =>
+        GetLanguageCode(Prompt.Select("Lingua a ser usada na traducao:",
+            new[] { EnglishLanguage, PortugueseLanguage, SpanishLanguage }));
+
+    public static string GetAnswerContinue() =>
+        Prompt.Select("Continuar?", new[] { "Sim", "Nao" });
+
+    private static string GetLanguageCode(string language)
+    {
+        switch (language)
+        {
+            case EnglishLanguage:
+                return EnglishCode;
+            case PortugueseLanguage:
+                return PortugueseCode;
+            case SpanishLanguage:
+                return SpanishCode;
+            default:
+                throw new ArgumentException("Lingua invalida!");
+        }
+    }
+    
+    private static void ShowErrorMessage(string message)
+    {
+        var oldForegroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = oldForegroundColor;
+        Console.WriteLine();
+    }
+}
---
+++ ConsoleAppTranslator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+using ConsoleAppTranslator.Inputs;
+
+Console.WriteLine("***** Utilizando o Azure Text Translation *****");
+Console.WriteLine();
+
+var endpointTextTranslation = InputHelper.GetEndpointTextTranslation();
+Console.WriteLine();
+
+var regionTextTranslation = InputHelper.GetRegionTextTranslation();
+Console.WriteLine();
+
+var subscriptionKey = InputHelper.GetSubscriptionKey();
+Console.WriteLine();
+
+using (var client = new HttpClient())
+{
+    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", regionTextTranslation);
+
+    string continuar;
+    do
+    {
+        var textToTranslate = InputHelper.GetTextToTranslate();
+        Console.WriteLine();
+
+        var targetLanguage = InputHelper.GetTargetLanguage();
+        Console.WriteLine();
+
+        var uri = $"{endpointTextTranslation}/translate?api-version=3.0&to={targetLanguage}";
+        Console.WriteLine($"Uri Translation: {uri}");
+
+        var requestBody = $$"""[ { "Text": "{{textToTranslate}}" } ]""";
+        Console.WriteLine($"Request Body: {requestBody}");
+
+        var response = await client.PostAsync(uri, new StringContent(requestBody,
+            Encoding.UTF8, "application/json"));
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var jsonResponseBody = JToken.Parse(responseBody);
+
+        Console.WriteLine();
+        Console.WriteLine($"Status Code: {(int)response.StatusCode} {response.StatusCode}");
+        Console.WriteLine($"Response Body: {responseBody}");
+        Console.WriteLine(
+            $"Conteudo traduzido: {jsonResponseBody.SelectToken("$[0].translations[0].text")!.Value<string>()}");
+        Console.WriteLine();
+
+        continuar = InputHelper.GetAnswerContinue();
+        Console.WriteLine();
+    } while (continuar == "Sim");
+
+    Console.WriteLine("Testes concluidos!");
+}
